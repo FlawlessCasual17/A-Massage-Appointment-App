9ffffcac6e5dc7bbad2a78c28dc965a364ea5e1f
FlawlessCasual17 <07e5297d5b@c0x0.com>
2024-12-02 22:38:55 -0700

    Update `src/app/utilities/button.tsx` , and Changed `tsconfig.json`
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- src/app/utilities/button.tsx
+++ src/app/utilities/button.tsx
@@ -3,9 +3,7 @@ import sty from './button.module.css'
 export default function Button() {
     return (
         <div className={`btn ${sty.btn_styling} hover:bg-violet-500 hover:border-violet-600`}>
-            <span className='relative leading-[1.5rem] uppercase text-ellipsis whitespace-nowrap'>
-                BOOK NOW
-            </span>
+            <input type='button' value='book now' className='relative leading-[1.5rem] uppercase text-ellipsis whitespace-nowrap' />
         </div>
     )
 }
