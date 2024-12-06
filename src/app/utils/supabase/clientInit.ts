9472828a5f48a7984dbe967c4ca4708f38ff1a8d
FlawlessCasual17 <07e5297d5b@c0x0.com>
2024-12-05 22:29:36 -0700

    Moving away from Supabase
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- src/app/utils/supabase/clientInit.ts
+++
@@ -1,8 +0,0 @@
-'use server'
-import { createClient } from './server'
-import { cookies } from 'next/headers'
-
-export async function initSupabaseClient() {
-    const cookieStore = cookies()
-    return createClient(cookieStore)
-}
